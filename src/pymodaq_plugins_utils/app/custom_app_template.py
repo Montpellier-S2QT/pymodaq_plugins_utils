9b1cc8cb1026db920545d116bcff985bcb9de685
Sébastien Weber <seba.weber@gmail.com>
2026-05-19 10:35:16 +0200

    modify packaging name, pyproject etc

--- src/pymodaq_plugins_utils/app/custom_app_template.py
+++ src/pymodaq_plugins_utils/app/custom_app_template.py
@@ -4,7 +4,7 @@ from pymodaq_gui import utils as gutils
 from pymodaq_utils.config import Config
 from pymodaq_utils.logger import set_logger, get_module_name
 
-# todo: replace here *pymodaq_plugins_template* by your plugin package name
+# todo: replace here *pymodaq_plugins_utils* by your plugin package name
 from pymodaq_plugins_template.utils import Config as PluginConfig
 
 logger = set_logger(get_module_name(__file__))
